d96ba78e6324f01d785810e9f5c8026609a382c3
chuchuanbao <chu.chuanbao@trs.com.cn>
2018-02-23 22:57:42 +0800

    实现windows下Python3下载相关图片的操作

--- BingWallpaper.py
+++ BingWallpaper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import urllib
+import urllib.request as request
 import os
 import time
 import datetime
@@ -11,21 +11,21 @@ import json
 date = str(datetime.date.today())
 
 who = getpass.getuser()
-path = '/home/'+who+'/Wallpaper/'+date+'/'
+path = 'F:\\图片\\'+who+'\\Wallpaper\\'+date+'\\'
 def exitsDir():
 	if not os.path.exists(path):
-		os.system('mkdir -p '+path)
+		os.system('mkdir '+path)
 
 def getImgUrl():
 	exitsDir()
 	#n这个参数的值为最多获取几张图片，实际获得的图片数可能少于n
 	url = "http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10"
-	conn = urllib.urlopen(url)
+	conn = request.urlopen(url)
 	resp = json.loads(conn.read())
 	imgUrls = []
 	#获取分辨率为1366X768的壁纸
 	for x in range(len(resp['images'])):
-		imgUrls.append(resp['images'][x]['url'].replace('1920x1080','1366x768'))
+		imgUrls.append("http://cn.bing.com"+(resp['images'][x]['url'].replace('1920x1080','1366x768')))
 	imgUrls = list(set(imgUrls))
 	#return resp['images'][0]['url'].replace('1920x1080','1366x768')
 	return imgUrls
@@ -34,8 +34,8 @@ def downloadImg():
 	imgUrls = getImgUrl()
 	for x in range(len(imgUrls)):
 		imgUrl = imgUrls[x]
-		image = open(name=path+date+"-"+str(x)+'.jpg',mode='wb')
-		conn = urllib.urlopen(url=imgUrl)
+		image = open(path+date+"-"+str(x)+'.jpg','wb')
+		conn = request.urlopen(url=imgUrl)
 		image.write(conn.read())
 		image.close()
 	return len(imgUrls)
@@ -43,19 +43,10 @@ def downloadImg():
 if __name__ == '__main__':
 	haveNetWork = False
 	while not haveNetWork:
-		msg = os.system('ping -c 1 cn.bing.com')
+		msg = os.system('ping -n 1 cn.bing.com')
 		if msg:
 			#每2分钟测试连一次网，看看是否连接成功！
 			time.sleep(60*2)
 		else:
 			haveNetWork = True
-	length = downloadImg()
-	a = 0
-	while True:
-		os.system('gsettings set org.gnome.desktop.background picture-uri "file://'+path+date+"-"+str(a)+'.jpg"')
-		a += 1
-		a = a%length
-		#print a
-		#每小时切换一次壁纸
-		time.sleep(60*60)
-	#os.system('gsettings set org.gnome.desktop.background picture-uri "file://'+path+date+'.jpg"')
+	downloadImg()
